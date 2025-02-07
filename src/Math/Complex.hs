db1cc242cd87e4c0d5407d8928c588b2d6ea014c
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-06 17:42:36 -0800

    feat: Add `Complex` type and `Eq` instance (#2)

--- src/Math/Complex.hs
+++ src/Math/Complex.hs
@@ -1,5 +1,11 @@
 -- SPDX-FileCopyrightText: Copyright Preetham Gujjula
 -- SPDX-License-Identifier: BSD-3-Clause
-module Math.Complex (Complex) where
 
-data Complex
+-- | Complex numbers, with alternative semantics to "Data.Complex".
+module Math.Complex (Complex (..)) where
+
+-- | A complex number type.
+--
+--   The complex number \(a + bi\) is represented by @a :+ b@.
+data Complex a = !a :+ !a
+  deriving (Eq)
