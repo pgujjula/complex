3dda326118ee6c40e5efc8a7c90a7a687280670a
Preetham Gujjula <gitcommit@mail.preetham.io>
2025-02-06 19:44:54 -0800

    test: Add unit test for `Eq` instance (#2)

--- test/Test/Math/Complex.hs
+++ test/Test/Math/Complex.hs
@@ -3,16 +3,27 @@
 
 module Test.Math.Complex (tests) where
 
-import Math.Complex ()
+import Math.Complex (Complex ((:+)))
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.HUnit (assertBool, testCase)
 
 tests :: TestTree
 tests =
   testGroup
     "Math.Complex"
-    [ placeholderTest
+    [ test_Eq
     ]
 
-placeholderTest :: TestTree
-placeholderTest = testCase "placeholder" ((1 :: Int) @?= 1)
+test_Eq :: TestTree
+test_Eq =
+  testCase "Eq instance" $ do
+    let checkEqual :: (Eq a) => (Complex a, Complex a) -> IO ()
+        checkEqual (x, y) = assertBool "found not equal" (x == y)
+
+        checkUnequal :: (Eq a) => (Complex a, Complex a) -> IO ()
+        checkUnequal (x, y) = assertBool "found equal" (x /= y)
+
+    mapM_ checkEqual [((0 :: Int) :+ 0, 0 :+ 0), (3 :+ 4, 3 :+ 4)]
+    mapM_
+      checkUnequal
+      [((0 :: Int) :+ 0, 1 :+ 0), ((-1) :+ 2, (-1) :+ 3), (3 :+ 4, 2 :+ 3)]
